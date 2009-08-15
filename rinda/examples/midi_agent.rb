1cbb5fc1af6c31d900c5df3c118a27b77418cf50
Jay McGavren <jay@mcgavren.com>
2009-08-15 03:07:36 -0700

    Added MIDI agent using MIDIator.

---
+++ rinda/examples/midi_agent.rb
@@ -0,0 +1,15 @@
+require 'rinda/rinda'
+require 'rubygems'
+require 'midiator'
+
+MY_URI = ARGV[0] || "druby://127.0.0.1:9999"
+DRb.start_service
+space = Rinda::TupleSpaceProxy.new(DRbObject.new(nil, MY_URI)) 
+
+midi = MIDIator::Interface.new
+midi.autodetect_driver
+
+loop do
+  key, value = space.take([/Integer/, Integer])
+  midi.play value, 0.01
+end
