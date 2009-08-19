54c9e715cb708088a87df70c6f5b7a6d8f0a7d83
Jay McGavren <jay@mcgavren.com>
2009-08-19 12:53:30 -0700

    Added Windows support to say_agent.

--- rinda/examples/say_agent.rb
+++ rinda/examples/say_agent.rb
@@ -1,10 +1,22 @@
 #OS X only.
-require 'rinda/rinda' 
+require 'rinda/rinda'
+require 'rbconfig'
 MY_URI = ARGV[0] || "druby://127.0.0.1:9999"
 DRb.start_service
 space = Rinda::TupleSpaceProxy.new(DRbObject.new(nil, MY_URI))
+
+def say(text)
+  case Config::CONFIG['host_os']
+  when /^win|mswin/
+    require 'win32/sapi5'
+    Win32::SpVoice.new.Speak(self.to_s)
+  when /darwin/
+    text.gsub!(/[^\w\s]/, '') #For safety.
+    `say #{text}` unless text.nil? or text.length < 1 or text =~ /^\s*$/
+  end
+end
+
 loop do
   key, value = space.take([/String/, String])
-  value.gsub!(/[^\w\s]/, '') #For safety.
-  `say #{value}` unless value.nil? or value.length < 1 or value =~ /^\s*$/
+  say(value)
 end
