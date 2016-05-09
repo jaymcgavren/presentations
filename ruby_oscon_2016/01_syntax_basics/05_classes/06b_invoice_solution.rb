0b6aa39ca3a6ceb27e888cd6977e89bfcc6c5318
Jay McGavren <jay@mcgavren.com>
2016-05-09 14:20:16 -0700

    Add classes exercise.

---
+++ ruby_oscon_2016/01_syntax_basics/05_classes/06b_invoice_solution.rb
@@ -0,0 +1,29 @@
+class Invoice
+
+  attr_accessor :subtotal, :tax_rate
+
+  def total
+    tax = subtotal * tax_rate
+    subtotal + tax
+  end
+
+end
+
+# OR...
+
+class Invoice
+
+  attr_accessor :subtotal, :tax_rate
+
+  def total
+    tax = @subtotal * @tax_rate
+    @subtotal + tax
+  end
+
+end
+
+
+invoice = Invoice.new
+invoice.subtotal = 10.00
+invoice.tax_rate = 0.1
+puts invoice.total # Result: 11.0
